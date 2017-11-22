3d42fb7319ed3be5292d2dfadce346ea553bc82a
Aurelien Naldi <aurelien.naldi@gmail.com>
2017-11-22 14:33:10 +0100

    Build the python module with clingo

--- clingo/build.sh
+++ clingo/build.sh
@@ -1,6 +1,6 @@
 
 # can't build from source with conda's old gcc
-cmake .
+cmake -DCLINGO_REQUIRE_PYTHON=ON .
 make
 
 # When building from source: enter the bin output folder
@@ -12,4 +12,6 @@ cp -p clasp clingo gringo lpconvert reify "$PREFIX/bin"
 # depending on build options, also copy the shared library (libclingo.so)
 cp -p lib*.so* "$PREFIX/lib"
 
+# add the python module
+cp -a python "$PREFIX/lib/python3.6"
 
