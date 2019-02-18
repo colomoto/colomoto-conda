c8614e7a4a7e12670c3ba74448e3ea9531d7494a
Aurelien Naldi <aurelien.naldi@gmail.com>
2019-02-18 16:40:30 +0100

    Build MaBoSS from git (include UpPMaBoSS prototype)

--- maboss/build.sh
+++ maboss/build.sh
@@ -4,9 +4,10 @@ cd engine/src
 make install
 make MAXNODES=128 install
 make MAXNODES=256 install
-mv ../pub/MaBoSS* ${PREFIX}/bin
+mv ../pub/MaBoSS  ${PREFIX}/bin
+mv ../pub/MaBoSS_*n  ${PREFIX}/bin
 cd ../..
 mkdir -p "${PREFIX}/share/MaBoSS"
-mv doc tutorial examples ${PREFIX}/share/MaBoSS/
+#mv doc tutorial examples ${PREFIX}/share/MaBoSS/
 mv tools/* ${PREFIX}/bin
 
