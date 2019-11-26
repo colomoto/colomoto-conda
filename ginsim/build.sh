b409b78a2bee294482c6c708997794924ac9382b
Aurelien Naldi <aurelien.naldi@gmail.com>
2019-11-26 10:53:52 +0100

    Also install the bioLQM launcher

--- ginsim/build.sh
+++ ginsim/build.sh
@@ -3,6 +3,6 @@
 mkdir -p "$PREFIX/opt"
 cp "${SRC_DIR}/GINsim-${PKG_VERSION}-SNAPSHOT-jar-with-dependencies.jar" "$PREFIX/opt/GINsim.jar"
 
-cp "${RECIPE_DIR}/GINsim" "$PREFIX/bin"
-chmod +x "$PREFIX/bin/GINsim"
+cp "${RECIPE_DIR}/GINsim" "${RECIPE_DIR}/bioLQM" "$PREFIX/bin"
+chmod +x "$PREFIX/bin/*"
 
