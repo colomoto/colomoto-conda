19fd644a756fe1ed584b7240fc7314e8ece68a67
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2019-11-26 12:38:03 +0100

    docker image for github action

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,5 @@ MAINTAINER CoLoMoTo Group <contact@colomoto.org>
 
 RUN conda install -y anaconda-client conda-build && conda clean -y --all
 
+ENTRYPOINT ["/entrypoint.sh"]
+COPY entrypoint.sh /entrypoint.sh
---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -ex
+set -o pipefail
+
+[ ! -z $INPUT_SUBDIR ] && cd $INPUT_SUBDIR
+conda config --set anaconda_upload yes
+conda build --user $INPUT_ANACONDA_USER --token $INPUT_ANACONDA_TOKEN .
