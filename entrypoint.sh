42a3c633b0462873d31e0ed0fdf643729e03ab79
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2019-11-29 12:37:44 +0100

    enforce channels

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,5 @@ set -o pipefail
 
 [ ! -z $INPUT_SUBDIR ] && cd $INPUT_SUBDIR
 conda config --set anaconda_upload yes
-conda build --user $INPUT_ANACONDA_USER --token $INPUT_ANACONDA_TOKEN .
+conda build --user $INPUT_ANACONDA_USER --token $INPUT_ANACONDA_TOKEN \
+    -c conda-forge -c colomoto .
