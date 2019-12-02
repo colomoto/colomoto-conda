10d3937a38cee06732365d4f21685d12c7df3e8a
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2019-12-02 23:03:04 +0100

    Explicit channels

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-set -ex
 set -o pipefail
-
+set -ex
 [ ! -z $INPUT_SUBDIR ] && cd $INPUT_SUBDIR
 conda config --set anaconda_upload yes
 conda build --user $INPUT_ANACONDA_USER --token $INPUT_ANACONDA_TOKEN \
