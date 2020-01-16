4dbaa283311ce0ccdef43f474a664c391310e5be
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2020-01-16 15:01:50 +0100

    docker image: add potassco channel

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,6 @@ set -ex
 [ ! -z $INPUT_SUBDIR ] && cd $INPUT_SUBDIR
 conda config --set anaconda_upload yes
 conda build --user $INPUT_ANACONDA_USER --token $INPUT_ANACONDA_TOKEN \
-    -c conda-forge -c colomoto .
+    -c conda-forge -c potassco -c colomoto \
+    $INPUT_CONDA_BUILD_OPTIONS \
+    .
