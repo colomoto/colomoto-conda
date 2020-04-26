934840bcb82decf1ab2563b74983072e4513aa74
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2020-04-26 18:13:38 +0200

    use defaults channel first

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@ set -o pipefail
 set -ex
 [ ! -z $INPUT_SUBDIR ] && cd $INPUT_SUBDIR
 conda build --user $INPUT_ANACONDA_USER --token $INPUT_ANACONDA_TOKEN \
-    -c conda-forge -c potassco -c colomoto \
+    -c defaults -c potassco -c conda-forge -c colomoto \
     $INPUT_CONDA_BUILD_OPTIONS \
     .
