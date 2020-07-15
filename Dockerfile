1936dc4b15e6dab6265e450f86efd799d7e17e84
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2020-07-15 23:26:52 +0200

    update builder

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM continuumio/miniconda3
 
-RUN conda install -y anaconda-client conda-build && conda clean -y --all
+RUN conda install -y anaconda-client conda-build conda-verify && conda clean -y --all
+RUN conda config --set channel_priority strict
 RUN conda config --set anaconda_upload yes
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -o pipefail
 set -ex
 [ ! -z $INPUT_SUBDIR ] && cd $INPUT_SUBDIR
-conda build --user $INPUT_ANACONDA_USER --token $INPUT_ANACONDA_TOKEN \
-    -c defaults -c potassco -c conda-forge -c colomoto \
+conda build --no-build-id --user $INPUT_ANACONDA_USER --token $INPUT_ANACONDA_TOKEN \
+    -c defaults -c conda-forge -c potassco -c colomoto \
     $INPUT_CONDA_BUILD_OPTIONS \
     .
