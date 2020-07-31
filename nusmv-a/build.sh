340e82bf9554c5caeebc7ab018a0007c5bd996b0
Aurelien Naldi <aurelien.naldi@gmail.com>
2020-07-31 16:04:41 +0200

    let NuSMV-a work side by side with NuSMV

--- nusmv-a/build.sh
+++ nusmv-a/build.sh
@@ -7,5 +7,7 @@ cmake -DBUILD_TESTS=OFF .. -Wno-dev
 make
 cp -a bin "$PREFIX"
 
-# TODO: rename the binary?
+# Rename the binary for use side-by-side with the regular NuSMV
+# ltl2smv is part of the regular NuSMV package
+cp -a bin/NuSMV "$PREFIX/bin/NuSMV-a"
 
