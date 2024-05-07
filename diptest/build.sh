ce3dcc68f43b23d8a52e4593e5d92d0c468983ee
Loïc Paulevé <loic.pauleve@labri.fr>
2024-05-07 15:45:52 +0200

    upgrade packages to python 3.12

--- diptest/build.sh
+++ diptest/build.sh
@@ -6,11 +6,11 @@ set -x
 echo $PY_VER
 
 if [ `uname` == Linux ]; then
-    if [ "$PY_VER" == "3.10" ]; then
-        pip install https://files.pythonhosted.org/packages/78/35/e3523f38e13a20e274ec49fc9cc57eac7d9704a1716af2f194883249a983/diptest-0.5.2-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
-    elif [ "$PY_VER" == "3.11" ]; then
-        pip install https://files.pythonhosted.org/packages/63/77/8aed702f2604487f2af217aa8ad8f4cd8a284a3469a28398f753681d8f84/diptest-0.5.2-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
-    else
-        exit 1
-    fi
+    case "$PY_VER" in
+        3.10) wheel="https://files.pythonhosted.org/packages/8e/81/74da486d9faa492d87b5ca48258cff30750ef91958cf10a09e2c56ebe6bf/diptest-0.8.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl" ;;
+        3.11) wheel="https://files.pythonhosted.org/packages/1f/5e/3f87ecae531897bda040540e2230ecd230dfbc5254e9388285342b972e32/diptest-0.8.0-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl" ;;
+        3.12) wheel="https://files.pythonhosted.org/packages/f5/d6/a5d0e89813d83c5718e5422b6719454898124878a351c8eb69bd53780266/diptest-0.8.0-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl" ;;
+        *) exit 1
+    esac
+    echo pip install --no-deps "$wheel"
 fi
