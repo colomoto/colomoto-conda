2c6e31c008219488cf953cacac52b86303ca7d97
Loïc Paulevé <loic.pauleve@labri.fr>
2024-11-08 11:14:08 +0100

    diptest

--- diptest/build.sh
+++ diptest/build.sh
@@ -7,9 +7,10 @@ echo $PY_VER
 
 if [ `uname` == Linux ]; then
     case "$PY_VER" in
-        3.10) wheel="https://files.pythonhosted.org/packages/8e/81/74da486d9faa492d87b5ca48258cff30750ef91958cf10a09e2c56ebe6bf/diptest-0.8.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl" ;;
-        3.11) wheel="https://files.pythonhosted.org/packages/1f/5e/3f87ecae531897bda040540e2230ecd230dfbc5254e9388285342b972e32/diptest-0.8.0-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl" ;;
-        3.12) wheel="https://files.pythonhosted.org/packages/f5/d6/a5d0e89813d83c5718e5422b6719454898124878a351c8eb69bd53780266/diptest-0.8.0-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl" ;;
+        3.10) wheel="https://files.pythonhosted.org/packages/49/50/679091e524551e1a2a86b273258a36e7a84db6a0f05d601c9b458a01f555/diptest-0.8.2-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl" ;;
+        3.11) wheel="https://files.pythonhosted.org/packages/d5/0b/9a3d354e78959e0bcd159317a593e2061d18cf091250bcf01a63e327006e/diptest-0.8.2-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl" ;;
+        3.12) wheel="https://files.pythonhosted.org/packages/dc/92/ad38a7a03e752c518cc54f96e8e008e7eebddf8074f78057c968a25a1555/diptest-0.8.2-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl" ;;
+        3.13) wheel="https://files.pythonhosted.org/packages/d4/0b/d75ae240eb87a327e53e7dd9a47b9fed0da0a8849456434ca976867d9136/diptest-0.8.2-cp313-cp313-manylinux_2_17_x86_64.manylinux2014_x86_64.whl" ;;
         *) exit 1
     esac
     echo pip install --no-deps "$wheel"
