d3f0b5307cf0ccfeb77ecbdcbaa3898414a8a691
Loïc Paulevé <loic.pauleve@labri.fr>
2024-11-13 23:39:10 +0100

    diptest on macos

--- diptest/build.sh
+++ diptest/build.sh
@@ -14,6 +14,15 @@ if [ `uname` == Linux ]; then
         *) exit 1
     esac
     pip install --no-deps "$wheel"
+elif [ `uname` == Darwin ]; then
+    case "$PY_VER" in
+        3.10) wheel="https://files.pythonhosted.org/packages/5e/ff/875497f4249495cb4dffcad782d74899a03d75b9af70703f87144c1ca7b7/diptest-0.8.2-cp310-cp310-macosx_12_0_arm64.whl" ;;
+        3.11) wheel="https://files.pythonhosted.org/packages/6c/5f/0a823a39520736add57d382eb9fc82dcc0a3245d3e9cb605903fb171c503/diptest-0.8.2-cp311-cp311-macosx_12_0_arm64.whl" ;;
+        3.12) wheel="https://files.pythonhosted.org/packages/06/a9/1e44e81031d221f11242795c5f82e55a5a4c81d362a1570aa3b1379b967f/diptest-0.8.2-cp312-cp312-macosx_12_0_arm64.whl" ;;
+        3.13) wheel="https://files.pythonhosted.org/packages/e4/cf/1c634d401666fb63afa4291dc5dd14303e24d4f00449bb1bf515302dbad7/diptest-0.8.2-cp313-cp313-macosx_12_0_arm64.whl" ;;
+        *) exit 1
+    esac
+    pip install --no-deps "$wheel"
 else
     pip install --no-deps --only-binary=:all: diptest
 fi
