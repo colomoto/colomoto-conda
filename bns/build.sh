1e9ab00071300686c268199e7c28cc62e7e81aa2
Loïc Paulevé <loic.pauleve@labri.fr>
2025-01-14 17:04:23 +0100

    bns: attempt macos build

--- bns/build.sh
+++ bns/build.sh
@@ -1,8 +1,12 @@
 
+mkdir -p "${PREFIX}"/bin
+
 # First build MiniSat
 cd ./MiniSat_v1.14
 make
 
 # build BNS itself
 cd ../src
-g++ ../MiniSat_v1.14/Solver.o -I../MiniSat_v1.14 bns.c -o "$PREFIX/bin/bns"
+
+
+$CXX ../MiniSat_v1.14/Solver.o -I../MiniSat_v1.14 bns.c -o "$PREFIX/bin/bns"
