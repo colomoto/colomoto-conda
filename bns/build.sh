197fc589a9798606449b4d51311ec34deaf71dc0
Loïc Paulevé <loic.pauleve@labri.fr>
2025-01-21 21:27:21 +0100

    bns on mac intel, take 11

--- bns/build.sh
+++ bns/build.sh
@@ -2,22 +2,18 @@ set -x
 
 mkdir -p "${PREFIX}"/bin
 
-env
-ls -l /usr/bin
-CXX=${CXX:-g++-14}
+CXX=${CXX:-g++}
 echo $CXX
 which $CXX
 $CXX -E -v
-which ld
-ld -v
 
 # First build MiniSat
 cd ./MiniSat_v1.14
-
 
 make CXX="$CXX -std=c++03" CFLAGS="$CFLAGS" Solver.o
 
 # build BNS itself
 cd ../src
 
-$CXX ../MiniSat_v1.14/Solver.o -I../MiniSat_v1.14 bns.c -o "$PREFIX/bin/bns"
+$CXX -std=c++03 ../MiniSat_v1.14/Solver.o -I../MiniSat_v1.14 bns.c -o "$PREFIX/bin/bns"
+ldd "$PREFIX/bin/bns"
