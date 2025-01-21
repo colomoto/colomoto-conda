5cca77ace86bac49d20ed1c20f7a2d93b1bbbaa7
Loïc Paulevé <loic.pauleve@labri.fr>
2025-01-21 15:41:33 +0100

    bns on macos intel, take 3

--- bns/build.sh
+++ bns/build.sh
@@ -1,3 +1,4 @@
+set -x
 
 mkdir -p "${PREFIX}"/bin
 
@@ -8,5 +9,10 @@ make
 # build BNS itself
 cd ../src
 
+echo $CXX
+which $CXX
+CXX=g++
+echo $CXX
+which $CXX
 
-$CXX ../MiniSat_v1.14/Solver.o -I../MiniSat_v1.14 bns.c -o "$PREFIX/bin/bns"
+g++ ../MiniSat_v1.14/Solver.o -I../MiniSat_v1.14 bns.c -o "$PREFIX/bin/bns"
